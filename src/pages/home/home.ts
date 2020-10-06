7be5407e3a080816b0c8572a3842aa1808696196
viswabnath <viswa.visu57@gmail.com>
2020-10-06 17:01:56 +0530

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Observable } from 'Rxjs/rx';
+import { Subscription } from "rxjs/RX";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  observableVar: Subscription
+  public text: string = 'START';
+  public texttwo: string = 'START';
+  result: any = 0;
+  resulttwo: any = 0;
+  constructor(public navCtrl: NavController) {  
+  }
+  public changeText(text) {
+    if (text === 'START') {
+      this.text = 'STOP';
+      this.observableVar = Observable.interval(10).subscribe(() => {
+        this.randomnumber();
+      })
+    } else {
 
-  constructor(public navCtrl: NavController) {
-
+      this.text = 'START';
+      this.observableVar.unsubscribe();
+    }
+  }
+  randomnumber() {
+    this.result = Math.floor(Math.random() * 10);
   }
 
+  public changeTexttwo(texttwo) {
+    if (texttwo === 'START') {
+      this.texttwo = 'STOP';
+      this.observableVar = Observable.interval(10).subscribe(() => {
+        this.randomnumbertwo();
+      })
+    } else {
+
+      this.texttwo = 'START';
+      this.observableVar.unsubscribe();
+    }
+  }
+  randomnumbertwo() {
+    this.resulttwo = Math.floor(Math.random() * 100);
+  }
 }
